679b475a2509f196550a76aac9219ad961e8ea3b
Rodrigo Cordoba <rodrigocordoba2296@gmail.com>
2023-01-24 17:45:28 -0300

    update2

--- interactivo.js
+++ interactivo.js
@@ -1,5 +1,12 @@
-var btn= document.getElementById("btn");
-btn.addEventListener("click", () => {
-Alert ("Usuario Notificado")
-console.log("Usuario Notificado")
+//FUNCIONALIDAD PARA EL BOTON 
+
+window.addEventListener("load", function() {
+  document.getElementById("btn"). addEventListener("click", function (){
+    alert("El usuario ha sido notificado, se comunicara con usted a la brevedad, Muchas gracias.");
+
+    //SE AÑADE UN CONSOLE LOG PARA QUE APAREZCA EN CONSOLA
+
+    console.log ("El usuario ha sido notificado, se comunicara con usted a la brevedad, Muchas gracias.")
   })
+})
+
